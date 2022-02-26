833a7f2eebc9814bc3332fe5b53e50757d0e69bb
Jae Wu <jaewuchun@gmail.com>
2022-02-26 02:46:12 -0800

    added writings

---
+++ pages/writing/hardship.tsx
@@ -0,0 +1,72 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+import Top from '../../components/Top'
+import styles from '../../styles/Home.module.css'
+
+const Hardship: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>jaewuchun</title>
+        <meta name="description" content="on the net" />
+      </Head>
+
+      <main className={styles.main}>
+        <Top />
+
+        <div className="page">
+          <p>
+            <strong>
+                new personal website
+            </strong>
+            <br/><br/>
+            writings {`->`} on hardship as a necessity for living<br/><br/>
+
+            January 30, 2022<br/><br/>
+
+            as i continue my struggle in turning thought into action and wrestle with the seeming independence of those two, i continue to return to the idea on the necessity of hardships
+            <br/><br/>
+            hardship necessitates action. although whether one decides to take action is another topic; one can either choose to take action or regress their Being in complacency—perhaps dictated by a function of their personality or the childhood makeup of one&apos;s values and character<br/><br/>
+
+            i digress. life requires hardships not for the sake of hardship but because life is meant to be lived, to contain both thinking and acting, and hardship compels one to act while thinking can be done by anyone reasonably mindful. thus the importance of hardship<br/><br/>
+
+            let me qualify this idea of hardship. hardship is not hard to come across life—our being innately and in essence limited as humans produce tragedy on a regular basis. tragedy is one form of hardships (tragedy precisely defined as emotionally harmful events outside of our reasonable control—and what is reasonable depends on the person, such as their prudence, personality, intelligence, and what not). Arguably everything destructive can be considered a tragedy, with exception of events that we have expectation for given a continuation a pattern of behaviour (but this distinction is as hard to make as the separation, or union, of predestination and free will).<br/><br/>
+
+            Hardship can also then be self destructive habits such as drinking on a regular basis, or choosing to insert oneself in environments they deem not helpful such as hanging out with morally ambiguous company (with morality here being the intent at good; people are seldom bad in how people qualify bad: morally ambiguous company can refer to your best friends, people you look up to, childhood friends)<br/><br/>
+
+            There are also levels to hardships—whether these levels are discrete or continuous is another layer of abstraction worth exploring another time. Spilling your Starbucks vanilla blonde latte is a hardship to the lowest degree. A hardship that is inconsequential (so in the discourse between continuous or discrete levels, perhaps all things are continuous but our interpretation of those things gives idea to the discrete—the resolution in which one is able to extract meaning from knowledge is the discrete). While a tragedy of a loved one or ones passing is arguably a hardship to the highest degree (assuming one&apos;s chief value is relationships). Such hardship is consequential, life altering.<br/><br/>
+
+            Yesterday i was binging through a show in Netflix called “The Guest.” Three protagonists are affected in their childhood from a demonic possession event and so in the present day, the timeline where the drama is played, the three of them converge paths and chase that evil spirit while risking their lives with relentless pursuit. One of the protagonist, a police, became a policewoman so she could catch her mother&apos;s killer. Another one, an Catholic priest, wanted to become an exorcist to exorcise his brother who murdered his family. Deft to say life imitates art (or the other way around—point is, both can tell us something about the other). Hardships can affect our lives like this.<br/><br/>
+
+            But that show is an example that I don&apos;t too closely relate with—it just adds to the idea of hardships affecting life (when the show explicitly has the lines “I became a police to catch my mother&apos;s murderer” the idea of hardship affecting ones actions is hard to miss). But what i&apos;m interested in needs to put that narrative in context with the comparative; would they have lived such powerfully meaningful lives if they had no hardship? Given a hypothetically perfect universe where tragedy is not inevitable, I&apos;d argue they wouldn&apos;t have, and such idea is the necessity of hardship; not that hardships affect people, but that hardships are necessary for action, for living.<br/><br/>
+
+            We must discern acute and chronic hardships, however. Anything negative that is chronic is fully harmful with no utility other than the fact that that precisely is a hardship. I&apos;m confusing you; an example: acute stress is good to get your ass moving, but chronic stress slowly chips away at your resolve and character. A person who is tragically unlucky to experience multiple and continual tragic events with no opportunity for them to respond to it to rise higher and grow stronger will be unbelievably beaten to the ground, buried six feet under. Hardships to be productive must require first a positive response and secondly enough time to see through the response to its natural conclusion. At this point, we could go into discussion about whether humans are able to do that, and i&apos;d naively say yes we can, we are able to—that God gives us only what we can handle gives hope to that truth.<br/><br/>
+
+            Anyways, I wonder what my next hardship will be. These days, my life is too comfortable. I can fail my term and not end up homeless. I can drop out of school and live with my parents at any time. I have enough money in my bank account can fund my UberEats for the foreseeable future. Comfortable does not mean fulfilling or happy (perhaps only because personally I don&apos;t value comfortability much—but maybe this is because i&apos;ve been comfortable).<br/><br/>
+
+            One of the greatest power I think that people have are being able to choose their own environments. Should more than hard work, one prioritize which environment they should put themselves in? “Do hard things” or “Get out of your comfort zone” are nice expressions which allude to this idea of experiencing obstacles for the purpose of overcoming the obstacles. But perhaps out of all the obstacles we should face in life, those that are unexpected and those we have no control over are the ones that affect us most meaningfully. Perhaps those sayings benefit from hindsight as we attribute unexpected hardships to stem from choices we made.<br/><br/>
+
+            Life only guarantees an adventure, and perhaps that is for the better. Recall that I wonder what my next hardship will be: implicitly, I neither desire or expect to be able to continue living the way I currently do. The real question is will I respond positively to my dear future hardship. Or is my current state a response in itself? A response of complacency, waiting for life to hit me like a truck?<br/><br/>
+
+            revisiting this idea<br/><br/><br/><br/>
+
+            recall that i am privileged to be comfortable. I think this privilege is granted to many people of my age and socioeconomic standing. That&apos;s not to excuse any negative implications of privilege by playing the relativistic moral game, saying “look they&apos;re also the same as me,” but a comment made to try partly explaining the struggles of many people of the same camp as me<br/><br/>
+
+            while writing a speech for my high school grad ceremony, I tried to think of the plight of our generation (so I can then say each of us has the antidote through x and y reasons to overcome this plight and lead humanity victorious — but metaphorically and with more oomf. I ended up writing about something else entirely).<br/><br/>
+
+            Comfortability seems to be a plight. Maybe not one that spans our entire generation uniquely, but definitely for a large section of it raised in the mid-upper middle class, in countries with abundant and redundant social safety nets, in a stable support network. For this camp, unless inevitable tragedy has struck already, the vast majority of us have not felt hardship either enough or to certain degrees that we would consider life changing. Nothing to test our resolve and discipline, nothing to pull us violently in some direction—either good or bad.<br/><br/>
+
+            I&apos;m not complaining. Again, this comfort is a privilege. I think, then, how else we find ourselves in driven action is through some transcendent aim. What we aim for, if there even is an aim, differs for everyone.<br/><br/>
+
+            I&apos;m inclined to believe that everyone will face unpreparable hardship at some time in their lives. Until then, we should (I should) take advantage of this privilege — which was not for free, which was given by hard work and hardships of those that came before us — and extend this quality of life from the head start some of us already have. Work compounds and our work compounds that of those who came before us. Chiefly, we (I) ought to work towards bettering relationships with others and with society with this privilege.<br/><br/>
+
+            That is where my thoughts lie. What ought and should are easier said than done — else why would should ever connote something that hasn&apos;t been done?
+          </p>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Hardship
