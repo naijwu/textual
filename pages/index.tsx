c81e161ba780d53efff85ea684dd3ecffa08e1c3
Jae Wu <jaewuchun@gmail.com>
2022-02-28 04:25:30 -0800

    cleaned

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,12 +19,9 @@ const Home: NextPage = () => {
           <p>
           based in Vancouver, I&apos;m a first-year science <Link href="/undergrad-at-ubc">undergrad @ ubc</Link>
           <br/>when not <Link href="/writing/studying">studying</Link>, I spend time as a developer for <Link href="/nwplus">nwPlus ubc</Link> and building <Link href="/democracy">democracy.so</Link>
+          <br/>Early highschool I delved into web design and built <Link href="/websites">several websites</Link> (for conferences, charities, businesses)
           <br/>I have a habit of starting projects randomly (most are small, <Link href="/weekend-projects">weekend projects</Link>)
           <br/>some <Link href="/projects">projects</Link> (more polished than others) include <Link href="https://internetspace.co">internetspace.co🡥</Link>, <Link href="https://wordseveryday.net">wordseveryday.net🡥</Link>, and <Link href="https://opengavel.app">opengavel.app🡥</Link>
-          <br/><br/>
-          starting middleschool, I delved into web design and built <Link href="/websites">several websites</Link> (for conferences, charities, businesses)
-          <br/>I even tried starting my own web design business in highschool
-          <br/>now I just manage a few of them under another sole proprietorship on the side
           <br/><br/>
           these days, I&apos;ve been learning of and building in crypto &amp; web3
           <br/>I&apos;m interested in the financial markets, <Link href="/writing">writing</Link>, and systems/infrastructure design
