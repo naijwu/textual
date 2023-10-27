3230f66d6b0491ffe70346fe820c2da55ca5aed1
Jae Wu <jaewuchun@gmail.com>
2023-10-26 17:42:49 -0700

    Update mainstream.tsx

--- pages/mainstream.tsx
+++ pages/mainstream.tsx
@@ -23,7 +23,7 @@ const Mainstream: NextPage = () => {
             <br/><br/>
             mainstream is an experiment lab for servicing internet native communities
             <br/><br/>
-            we've closed our pre-seed round (led by Lemniscap, joined by Samsung Next + many amazing angels) to focus on onboarding organizations to the platform
+            we&apos;ve closed our pre-seed round (led by Lemniscap, joined by Samsung Next + many amazing angels) to focus on onboarding organizations to the platform
             <br/><br/>
             mainstream client is a modular, end-to-end governance platform for DAOs. Discuss proposals, have off-chain voting (Moloch, Snapshot, etc.), and then on-chain voting (Bravo, etc.) all in a package that makes sense and is efficient
             <br/><br/>
@@ -32,7 +32,7 @@ const Mainstream: NextPage = () => {
             pager for protocols gives community core members a convenient way to blast updates to community members
             <br/><br/>
             visit our client landing <Link href="https://mainstream.so">mainstream.so🡥</Link><br/>
-            visit our Pager landing <Link href="https://getpager.xyz">mainstream.so🡥</Link>
+            visit our Pager landing <Link href="https://getpager.xyz">getpager.xyz🡥</Link>
           </p>
         </div>
       </main>
